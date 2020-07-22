6608ad535695ecae2483e32f0f02bd7fef954d09
yymin1022 <yymin1022@gmail.com>
2020-07-22 14:12:19 +0900

    Add Log Utils Class

--- src/StoneManager.java
+++ src/StoneManager.java
@@ -1,5 +1,15 @@
+import javax.bluetooth.BluetoothStateException;
+import javax.bluetooth.LocalDevice;
+
 public class StoneManager{
     public static void main(String[] arg0){
         System.out.println("Hello, World!");
+
+        LocalDevice btDevice;
+        try{
+            btDevice = LocalDevice.getLocalDevice();
+        }catch(BluetoothStateException e){
+            System.out.println(String.format("ERROR : %s", e.toString()));
+        }
     }
 }
