82b488b97b77edb39a355857e35037f5eb9b417e
yymin1022 <yymin1022@gmail.com>
2020-07-25 01:02:29 +0900

    Update Main : Improved Structure

--- src/StoneManager.java
+++ src/StoneManager.java
@@ -1,13 +1,9 @@
-import Controller.BTController;
 import Controller.UIController;
-import Utils.Log;
 import View.StoneManagerView;
-
-import javax.bluetooth.*;
 
 public class StoneManager{
     public static void main(String[] arg0) {
         StoneManagerView stoneManagerView = new StoneManagerView();
-        UIController uiController = new UIController(stoneManagerView);
+        new UIController(stoneManagerView);
     }
 }
