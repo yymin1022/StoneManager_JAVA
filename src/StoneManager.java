0e1445d3fd4846218c1b0b9545f34f2bfc95a51d
yymin1022 <yymin1022@gmail.com>
2020-07-24 20:39:49 +0900

    Update View : Default Layout Initialize

--- src/StoneManager.java
+++ src/StoneManager.java
@@ -1,24 +1,26 @@
 import Controller.BTController;
 import Utils.Log;
+import View.StoneManagerView;
 
 import javax.bluetooth.*;
 
 public class StoneManager{
     public static void main(String[] arg0) {
-        Log log = new Log();
-
-        LocalDevice btDevice;
-        try {
-            btDevice = LocalDevice.getLocalDevice();
-
-            log.printLog(2, "MAC Addr is " + btDevice.getBluetoothAddress());
-        } catch (BluetoothStateException e) {
-            log.printLog(1, e.toString());
-        }
-
-        BTController btController = new BTController();
-        btController.start();
-
-        while(true);
+        new StoneManagerView();
+//        Log log = new Log();
+//
+//        LocalDevice btDevice;
+//        try {
+//            btDevice = LocalDevice.getLocalDevice();
+//
+//            log.printLog(2, "MAC Addr is " + btDevice.getBluetoothAddress());
+//        } catch (BluetoothStateException e) {
+//            log.printLog(1, e.toString());
+//        }
+//
+//        BTController btController = new BTController();
+//        btController.start();
+//
+//        while(true);
     }
 }
--- src/View/StoneManagerView.java
+++ src/View/StoneManagerView.java
@@ -1,9 +1,46 @@
 package View;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class StoneManagerView{
+    public ButtonGroup groupRadio;
     public JButton btnSave;
     public JFrame frameMain;
+    public JPanel panelButton;
     public JRadioButton radioAurora, radioCandle, radioFirefly, radioRGB, radioWave;
+
+    public StoneManagerView(){
+        frameMain = new JFrame();
+        panelButton = new JPanel(new GridLayout(6, 1));
+
+        btnSave = new JButton("저장");
+
+        radioAurora = new JRadioButton("오로라");
+        radioCandle = new JRadioButton("촛불");
+        radioFirefly = new JRadioButton("반딧불");
+        radioRGB = new JRadioButton("단일 색상");
+        radioWave = new JRadioButton("파도");
+
+        groupRadio = new ButtonGroup();
+        groupRadio.add(radioAurora);
+        groupRadio.add(radioCandle);
+        groupRadio.add(radioFirefly);
+        groupRadio.add(radioRGB);
+        groupRadio.add(radioWave);
+
+        panelButton.add(radioAurora);
+        panelButton.add(radioCandle);
+        panelButton.add(radioFirefly);
+        panelButton.add(radioRGB);
+        panelButton.add(radioWave);
+        panelButton.add(btnSave);
+
+        frameMain.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frameMain.setSize(200, 200);
+        frameMain.setTitle("STONE Manager");
+        frameMain.setVisible(true);
+
+        frameMain.getContentPane().add(panelButton);
+    }
 }
