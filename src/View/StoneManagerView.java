17468447137d3f0baaa02e3086ac6163aa343f0a
yymin1022 <yymin1022@gmail.com>
2020-07-24 20:27:54 +0900

    Add Swing Layout Resources to View Class

--- src/View/StoneManagerView.java
+++ src/View/StoneManagerView.java
@@ -1,4 +1,9 @@
 package View;
 
-public class StoneManagerView {
+import javax.swing.*;
+
+public class StoneManagerView{
+    public JButton btnSave;
+    public JFrame frameMain;
+    public JRadioButton radioAurora, radioCandle, radioFirefly, radioRGB, radioWave;
 }
